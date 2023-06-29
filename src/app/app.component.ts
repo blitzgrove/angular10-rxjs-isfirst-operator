d70deabdc704e972ccc3d27964375eb4ed4354b5
blitzgrove <61056521+blitzgrove@users.noreply.github.com>
2023-06-29 09:28:49 +0200

    combineLatest() -> combineLatest([])

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,30 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
-import { timer, Observable, Subject, combineLatest } from "rxjs";
-import { tap, repeat, switchMap, finalize, takeUntil } from "rxjs/operators";
+import { timer, Observable, Subject, combineLatest } from 'rxjs';
+import { tap, repeat, switchMap, finalize, takeUntil } from 'rxjs/operators';
 
 export const isFirst = (predicate: any) => {
   let first = true;
   return <T>(source: Observable<T>) => {
     return source.pipe(
       tap({
-        next: _ => {
+        next: (_) => {
           if (first) {
             predicate();
             first = false;
           }
-        }
+        },
       })
     );
   };
 };
 
 @Component({
-  selector: "my-app",
+  selector: 'my-app',
   template: `
     <button (mouseup)="start$.next()">Start</button>
     <button (mouseup)="stop$.next()">Stop</button>
-  `
+  `,
 })
 export class AppComponent {
   start$ = new Subject<any>();
@@ -33,17 +33,17 @@ export class AppComponent {
   constructor() {
     this.start$
       .pipe(
-        tap(_ => console.log("Start...")),
-        switchMap(_ => combineLatest(timer(2000, 1000), timer(3000, 500))),
-        isFirst(_ => {
-          console.log("first");
+        tap((_) => console.log('Start...')),
+        switchMap((_) => combineLatest([timer(2000, 1000), timer(3000, 500)])),
+        isFirst((_) => {
+          console.log('first');
         }),
         takeUntil(this.stop$),
-        finalize(() => console.log("...Stop")),
+        finalize(() => console.log('...Stop')),
         repeat()
       )
       .subscribe({
-        next: r => console.log("inside subscription:", r)
+        next: (r) => console.log('inside subscription:', r),
       });
   }
 }
